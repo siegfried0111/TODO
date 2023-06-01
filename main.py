53f2d11cc63cb7ea02a3e31861d1dd175b85d842
hjluo <106792087+siegfried0111@users.noreply.github.com>
2023-06-01 21:53:26 +0800

    auth.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routes import todo, user
+from routes import todo, auth
 
 app = FastAPI()
 
 app.include_router(todo.route)
-app.include_router(user.route)
+app.include_router(auth.route)
 
 
 
--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 from typing import Annotated
 from pydantic import BaseModel, Field
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from database import User, SessionLocal
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
+from jose import jwt
 
 route = APIRouter(prefix='/auth', tags=['authenticate'])
 
@@ -34,6 +37,13 @@ ctx = CryptContext(
     default="pbkdf2_sha256"
 )
 
+Secret_Key = 'Qekkvw51cqwfJy8jQx/rNbmewkhXRAK7oV43wbnbJvk='
+Algo = 'HS256'
+
+
+def jwt_token(payload):
+    return jwt.encode(claims=payload, key=Secret_Key, algorithm=Algo)
+
 
 @route.post('', status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, body: UserRequest):
@@ -53,5 +63,24 @@ async def create_user(db: db_dependency, body: UserRequest):
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f'{e}')
 
 
+@route.post('/token')
+async def get_token(form_body: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user_data = db.query(User).filter(User.username == form_body.username).first()
+    if user_data is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
+    if not ctx.verify(form_body.password, user_data.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
+    payload = {
+       'sub': user_data.username,
+       'id': user_data.id,
+       'exp': datetime.now() + timedelta(minutes=10)
+    }
+    token = jwt.encode(claims=payload, key=Secret_Key, algorithm=Algo)
+    return {'token': token, 'type': 'bearer'}
 
-# @route.get('', status_code=)
+# payload = {
+#        'sub': "A",
+#        'id': 1,
+#        'exp': datetime.now() + timedelta(minutes=10)
+#     }
+# print(jwt.encode(claims=payload, key=Secret_Key, algorithm=Algo))
